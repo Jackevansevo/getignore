65f3c1142256968509669f17231332f67dccef56
Jackevansevo <jack@evans.gb.net>
2017-10-26 09:59:16 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='getignore',
 
-    version='0.1.2',
+    version='0.1.3',
 
     description='Command line utility to download common .gitignore files',
     long_description=long_description,
